7510f52763ea217f6440cc8b6d895db8e5a9f2ae
Randall Evan McClellan <vanlellan@gmail.com>
2018-07-03 23:38:55 -0400

    minor changes to client

--- client.py
+++ client.py
@@ -1,14 +1,15 @@
 import socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('localhost', 8089))
+#clientsocket.connect(('localhost', 8089))
+clientsocket.connect(('192.168.1.109', 8089))
 
 
 while True:
 	message = raw_input("Type message: ")
-	if message == 'exit':
-		break
 	clientsocket.send(message)
 	reply = clientsocket.recv(1024)
 	if len(reply) > 0:
 		print reply
+	if message in ('exit','kill','quit'):
+		break
