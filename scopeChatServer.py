1a9342d6c621b5e2848379cc212c5c92a0c43738
Randall Evan McClellan <vanlellan@gmail.com>
2018-07-08 23:55:00 -0400

    tweaks to scopeChatServer.py
    -remove kill swtich completely
    -clean up client connection thread closing
    	-assumes recv empty byte string when connection is lost

--- scopeChatServer.py
+++ scopeChatServer.py
@@ -2,17 +2,16 @@ import socket
 import thread
 import Queue as qu
 
-def clientListen(aConnection, aQ, aKillSwitch):
-    while not aKillSwitch[0]:
+def clientListen(aConnection, aQ):
+    while True:
         buf = aConnection.recv(64)
         if buf == '':	#kill thread if empty buffer is sent (usually due to broken connection)
+            print "Client left."
             break
         aQ.put(buf)
-        if buf in ('kill','quit','exit'):
-            aKillSwitch[0] = True
 
 def clientsBroadcast(aDict, aQ, aKillSwitch):
-    while not aKillSwitch[0]:
+    while True:
         message = aQ.get()
         aKillSwitch[1] += 1
         for aC in aDict:
@@ -25,7 +24,7 @@ serversocket.bind(('', 5406))
 serversocket.listen(5) # become a server socket, maximum 5 connections
 
 clientDict = {}
-killSwitch = [False,0]	#first element is killswitch, second is pause (make this a "control dictionary" or "control object")
+killSwitch = [False,0]	#first element is killswitch (now unused), second is pause (make this a "control dictionary" or "control object")
 q = qu.Queue()
 
 broadcaster = thread.start_new_thread(clientsBroadcast,(clientDict, q, killSwitch))
@@ -34,7 +33,7 @@ print "Ready \n"
 
 while True:
     connection, address = serversocket.accept()
-    newClient = thread.start_new_thread(clientListen,(connection, q, killSwitch))
+    newClient = thread.start_new_thread(clientListen,(connection, q))
     print "Got new client! address = ", address
     while killSwitch[1] != 0:
         print "waiting for unpause..."
